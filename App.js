81504f62751f8413fc87fd03e2f87fc1d2d8e57f
Adrian Marvel <marvel.adrian@yahoo.com>
2021-10-08 17:12:09 +0700

    http request

--- App.js
+++ App.js
@@ -1,50 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View, ActivityIndicator, FlatList } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, ActivityIndicator, FlatList } from 'react-native';
+import Axios from 'axios';
 
-export default function App() {
-
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-
-  const getMovies = async () => {
-     try {
-      const response = await fetch('http://localhost/mahasiswa/api/read.php');
-      const json = await response.json();
-      setData(json.movies);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
+export default App = () => {
+  var request = new XMLHttpRequest();
+request.onreadystatechange = (e) => {
+  if (request.readyState !== 4) {
+    return;
   }
 
-  useEffect(() => {
-    getMovies();
-  }, []);
+  if (request.status === 200) {
+    console.log('success', request.responseText);
+  } else {
+    console.warn('error');
+  }
+};
 
-  fetch('http://localhost/mahasiswa/api/read.php');
+request.open('GET', 'http://192.168.1.102/mahasiswa/api/read.php');
+request.send();
 
+const renderItem = ({ item }) => (
+    <Item title={item.title} />);
   return (
-    <View style={{ flex: 1, padding: 24 }}>
-      {isLoading ? <ActivityIndicator/> : (
-        <FlatList
-          data={data}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => (
-            <Text>{item.id}, {item.nim}</Text>
+    <SafeAreaView>
+      <FlatList
+        data={request.responseText}
+        keyExtractor={item => item.id}
+        renderItem={({ renderItem }) => (
+            <Text>{item(1)}</Text>
           )}
-        />
-      )}
-    </View>
-  )
-
-/*const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-}*/
+        
+      />
+    </SafeAreaView>
+  );
 };
