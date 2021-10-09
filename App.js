f603666bd9c64431a76b254fec46fee0911f8609
Adrian Marvel <marvel.adrian@yahoo.com>
2021-10-09 21:47:03 +0700

    api read

--- App.js
+++ App.js
@@ -1,37 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useEffect, useState} from 'react';
-import { SafeAreaView, StyleSheet, Text, View, ActivityIndicator, FlatList } from 'react-native';
-import Axios from 'axios';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default App = () => {
-  var request = new XMLHttpRequest();
-request.onreadystatechange = (e) => {
-  if (request.readyState !== 4) {
-    return;
+class App extends Component{
+    constructor(props){
+      super(props);
+      this.state = {
+        nama:'',
+        alamat:'',
+        listData:[],
+        idEdit:null
+      };
+      this.url = "http://192.168.1.102/mahasiswa/api/read.php";
+    }
+    componentDidMount(){
+      this.ambilListData();
+    }
+    async ambilListData(){
+      await fetch(this.url)
+      .then((response)=>response.json())
+      .then((json)=>{
+        console.log('Hasil yang didapat: '+JSON.stringify(json.data.result));
+        this.setState({listData:json.data.result});
+      })
+      .catch((error)=>{
+        console.log(error);
+      })
+    }
+
+
+    render(){
+      return(
+        <View>
+          {
+            this.state.listData.map((val,index)=>
+            <View key={index}>
+              <Text>{val.nama}</Text>
+            </View>)
+          }
+        </View>
+      )
+    }
   }
+  
+  
 
-  if (request.status === 200) {
-    console.log('success', request.responseText);
-  } else {
-    console.warn('error');
-  }
-};
-
-request.open('GET', 'http://192.168.1.102/mahasiswa/api/read.php');
-request.send();
-
-const renderItem = ({ item }) => (
-    <Item title={item.title} />);
-  return (
-    <SafeAreaView>
-      <FlatList
-        data={request.responseText}
-        keyExtractor={item => item.id}
-        renderItem={({ renderItem }) => (
-            <Text>{item(1)}</Text>
-          )}
-        
-      />
-    </SafeAreaView>
-  );
-};
+export default App;
+/*export default App = () => {
+    
+  
+}*/
