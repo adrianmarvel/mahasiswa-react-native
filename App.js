16934ac674727e261aaa8e0bd81bb96ac4f71991
Adrian Marvel <marvel.adrian@yahoo.com>
2021-10-14 18:38:11 +0700

    add button

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, Modal } from 'react-native';
+import { FAB } from 'react-native-paper';
 
 class App extends Component{
     constructor(props){
@@ -12,7 +13,7 @@ class App extends Component{
         listData:[],
         idEdit:null
       };
-      this.url = "http://192.168.1.103/mahasiswa/api/read.php";
+      this.url = "http://192.168.1.102/mahasiswa/api/read.php";
     }
     componentDidMount(){
       this.ambilListData();
@@ -28,23 +29,36 @@ class App extends Component{
         console.log(error);
       })
     }
-
+  
 
     render(){
       return(
-        <View style={styles.container}>
-          <Text style={styles.font}>Data Mahasiswa</Text>
-          {
-            this.state.listData.map((val,index)=>
-            <View style={styles.data} key={index}>
-              <Text>Nama : {val.nama}</Text>
-              <Text>Alamat : {val.alamat}</Text>
-              <Text>NIM : {val.nim}</Text>
-              <Text>Jurusan : {val.jurusan}</Text>
-              <Text>No. HP : {val.telp}</Text>
-            </View>)
-          }
-        </View>
+        <SafeAreaView style={styles.saveAreaView}>
+          <ScrollView style={styles.scrollView}>
+            <View style={styles.container}>
+              <Text style={styles.font}>Data Mahasiswa</Text>
+              {
+                this.state.listData.map((val,index)=>
+                <View style={styles.data} key={index}>
+                  <Text>Nama : {val.nama}</Text>
+                  <Text>Alamat : {val.alamat}</Text>
+                  <Text>NIM : {val.nim}</Text>
+                  <Text>Jurusan : {val.jurusan}</Text>
+                  <Text>No. HP : {val.telp}</Text>
+                </View>)
+              }
+            </View>
+          </ScrollView>
+          <View>
+            <FAB
+              style={styles.fab}
+              medium
+              icon="plus"
+              color="white"
+              onPress={() => console.log('Pressed')}
+            />
+          </View>
+        </SafeAreaView>
       )
     }
   }
@@ -53,12 +67,24 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 100
   },
+  saveAreaView: {
+    height: 600
+  },
+  scrollView: {
+  },
+  
   font: {
     textAlign: 'center',
     fontSize: 32
   },
   data: {
     marginTop: 20
+  },
+  fab: {
+    position: 'absolute',
+    marginRight: 30,
+    right: 0,
+    bottom: 0,
   }
 });
 
